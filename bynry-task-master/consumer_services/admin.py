2c4ae63130dec522d97f56b233cb7c202e130b6e
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2024-04-18 22:19:35 +0530

    Delete bynry-task-master directory

--- bynry-task-master/consumer_services/admin.py
+++
@@ -1,7 +0,0 @@
-# consumer_services/admin.py
-
-from django.contrib import admin
-from .models import ServiceRequest, CustomerAccount
-
-admin.site.register(ServiceRequest)
-admin.site.register(CustomerAccount)
--- bynry-task-master/consumer_services/forms.py
+++
@@ -1,9 +0,0 @@
-# consumer_services/forms.py
-
-from django import forms
-from .models import ServiceRequest
-
-class ServiceRequestForm(forms.ModelForm):
-    class Meta:
-        model = ServiceRequest
-        fields = ['request_type', 'details', 'attachment']
